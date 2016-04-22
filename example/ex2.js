b506c5e7bd3d45dc085a59056105acdce9f80e25
Yuki Akiyama <you2197901@gmail.com>
2016-04-22 17:47:09 +0900

    Update ex2.js

--- example/ex2.js
+++ example/ex2.js
@@ -1,11 +1,13 @@
 'use strict'
 const util = require('../');
 const MIN_FEE = 10000;
+const KEY_NUM = 3;
+const KEY_SIGN_NUM = 2;
 
 const getMultisigTransactionFees = (input_num, m, n) => util.getCurrentBytePerSatoshi().then( satoshi =>
     Math.max(util.tx_calc_fee( util.tx_calc_byte(util.p2sh_calc_input_byte(m,n), input_num, 1), satoshi ), MIN_FEE)
 )
 
-getMultisigTransactionFees(2,3,1).then(console.log)
+getMultisigTransactionFees(KEY_SIGN_NUM, KEY_NUM, 1).then(console.log)
 
 
