2fc23672ad4c65c0d0fd2b0e3b95269a4d5378f9
you21979 <you2197901@gmail.com>
2016-12-28 16:41:13 +0900

    add doc

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 'use strict'
-
 var TxFees = exports;
 
 TxFees.BASE_BYTE_PER_SATOSHI = 10;
 
+TxFees.getBaseBytePerSatoshi = function(){ return TxFees.BASE_BYTE_PER_SATOSHI }
+
+TxFees.p2pkh_calc_input_byte = function(){ return 148 }
+
 TxFees.p2sh_calc_input_byte = function(m, n){
     return (74 * m) + (34 * n) + 49
-}
-
-TxFees.p2pkh_calc_input_byte = function(){
-    return 148
 }
 
 TxFees.tx_calc_byte = function(input_byte, input_num, output_num){
@@ -19,6 +18,4 @@ TxFees.tx_calc_byte = function(input_byte, input_num, output_num){
 TxFees.tx_calc_fee = function(byte, byte_per_satoshi){
     return (byte * byte_per_satoshi)
 }
-
-TxFees.getBaseBytePerSatoshi = function(){ return TxFees.BASE_BYTE_PER_SATOSHI }
 
